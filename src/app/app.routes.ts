1251fe6fc0ee7b0f4f63851785d1697ba8c381fc
Nicho <nicholasleonard04@gmail.com>
2025-10-30 00:09:17 +0700

    rapiin file n belajar basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Contact } from './pages/contact/contact';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Angular',
+    component: Home,
+  },
+  {
+    path: 'contact',
+    title: 'Contact',
+    component: Contact,
+  },
+  {
+    path: 'about',
+    title: 'About',
+    component: About,
+  },
+];
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -5,6 +5,5 @@ import { RouterLink } from '@angular/router';
   selector: 'app-navbar',
   imports: [RouterLink],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
 })
 export class Navbar {}
